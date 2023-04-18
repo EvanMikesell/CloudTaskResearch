aeeec03df583ce444c81c310f68d7f4d9e91c34d
Evan Mikesell <mikesellevan@gmail.com>
2023-04-18 16:14:28 -0700

    fixing results calculations
    results should now correctly print out the variance in loads

--- HAMM Test.py
+++ HAMM Test.py
@@ -2,19 +2,14 @@ import random
 import math
 
 """
-Testing to see if the HAMM Algorithm (Hybrid Algorithm of Min-Min and Max-Min) can be improved.
-We will look to see if the virtual machines have balanced loads at the end of the algorithm.
-Can look into ways to balance these virtual machines.
+Compares the performance of the HAMM algorithm and the EHAMM algorithm.
+We are concerned with the makespan as well as the load variance between machines.
 """
 
 
-def hamm():
-    num_tasks = 1000
-    num_vms = 6
-    tasks = [random.randint(1, 1000) for x in range(0, num_tasks)]
+def ehamm(num_tasks=1000, num_vms=6, task_size_cap=1000):
+    tasks = [random.randint(1, task_size_cap) for x in range(0, num_tasks)]
     vms = [[] for x in range(0, num_vms)]  # can set however many VMs you want
-    #tasks = [20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 1, 20, 20, 30, 40, 50, 60, 70, 80, 90]
-    #vms = [[] for x in range(0, 3)]
 
     while tasks:
         avg = sum(tasks) / len(tasks)
@@ -33,42 +28,42 @@ def hamm():
         else:
             min_min(tasks, vms)
 
-    print(vms)
-    rescheduled_vms = reschedule(vms)
-    print_results(vms, rescheduled_vms)
-    print(vms)
-    print(rescheduled_vms)
+    # find original makespan and variance in loads
+    makespan, variance = get_results(vms)
+    print("HAMM Scheduling:")
+    print("Makespan:", makespan)
+    print("Variance in load sizes:", variance)
 
+
+    # Compare to the rescheduled results
+    rescheduled_vms = reschedule(vms)
+    rescheduled_makespan, rescheduled_variance = get_results(rescheduled_vms)
+    print("EHAMM Scheduling")
+    print("Makespan:", rescheduled_makespan)
+    print("Variance in load sizes:", rescheduled_variance)
+    print()
 
 def reschedule(vms):
     high_load, low_load = high_and_low_split(vms)
 
-    print("HIGH VS LOW LOAD")
-    print(high_load)
-    print(low_load)
-
     while high_load:
         high_load.sort()
-        for i in range(len(high_load)-1, -1, -1):
-            high_load_machine = high_load[i]
-            smallest_task = min(high_load_machine)
-            smallest_load_machine = min(low_load)
-            print(high_load_machine, smallest_task)
+        high_load_machine = max(high_load)
+        smallest_task = min(high_load_machine)
+        smallest_load_machine = min(low_load)
 
-            # find average
-            smallest_load_machine_size = sum(smallest_load_machine)
-            high_load_machine_size = sum(high_load_machine)
-            difference = high_load_machine_size - smallest_load_machine_size
+        # find average
+        smallest_load_machine_size = sum(smallest_load_machine)
+        high_load_machine_size = sum(high_load_machine)
+        difference = high_load_machine_size - smallest_load_machine_size
 
-            difference_after_rescheduling = (high_load_machine_size - smallest_task) - (smallest_load_machine_size + smallest_task)
-            print(difference, difference_after_rescheduling)
-            if abs(difference) > abs(difference_after_rescheduling):
-                print("rescheduling")
-                smallest_load_machine.append(smallest_task)
-                high_load_machine.remove(smallest_task)
-            else:
-                low_load.append(high_load_machine)
-                high_load.remove(high_load_machine)
+        difference_after_rescheduling = (high_load_machine_size - smallest_task) - (smallest_load_machine_size + smallest_task)
+        if abs(difference) > abs(difference_after_rescheduling):
+            smallest_load_machine.append(smallest_task)
+            high_load_machine.remove(smallest_task)
+        else:
+            low_load.append(high_load_machine)
+            high_load.remove(high_load_machine)
     return low_load
 
 
@@ -86,36 +81,6 @@ def high_and_low_split(vms, threshold=0.02):
         else:
             low_load.append(vms[i])
     return high_load, low_load
-
-
-def print_results(original_vms, rescheduled_vms):
-    original_makespan = calculate_makespan(original_vms)
-    rescheduled_makespan = calculate_makespan(rescheduled_vms)
-    original_variance = calculate_load_balance(original_vms)
-    rescheduled_variance = calculate_load_balance(rescheduled_vms)
-
-    print("Original Makespan:", original_makespan)
-    print("Rescheduled Makespan:", rescheduled_makespan)
-
-    print("Original Variance:", original_variance)
-    print("Rescheduled Variance:", rescheduled_variance)
-
-
-
-def calculate_makespan(vms):
-    makespan = 0
-    for machine in vms:
-        makespan = max(makespan, sum(machine))
-    return makespan
-
-
-def calculate_load_balance(vms):
-    # calculate average load
-    loads = []
-    for vm in vms:
-        avg = sum(vm) / len(vm)
-        loads.append(avg)
-    return -1
 
 
 def max_min(tasks, vms):
@@ -138,6 +103,25 @@ def add_to_min_vm(val, vms):
             vm_to_assign = i
             min_vm = sum(vm)
     vms[vm_to_assign].append(val)
+
+def calculate_makespan(vms):
+    makespan = 0
+    for machine in vms:
+        makespan = max(makespan, sum(machine))
+    return makespan
+
+
+def calculate_load_balance(vms):
+    n = len(vms)
+    mean_size = sum(len(lst) for lst in vms) / n
+    variance = sum((len(lst) - mean_size) ** 2 for lst in vms) / (n - 1)
+    return variance
+
+
+def get_results(vms):
+    makespan = calculate_makespan(vms)
+    load_balance = calculate_load_balance(vms)
+    return makespan, load_balance
 
 
 def print_vm_info(num_tasks, num_vms, vms):
@@ -163,4 +147,5 @@ def print_vm_info(num_tasks, num_vms, vms):
     print("Average Task Size: ", average_task_size)
 
 
-hamm()
+if __name__ == '__main__':
+    ehamm()
